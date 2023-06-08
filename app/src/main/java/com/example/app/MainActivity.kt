a09da6ebf8e6e931adc12f04b74ef0998ece5270
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-06-08 23:34:28 +0700

    8/6

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -8,8 +8,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
