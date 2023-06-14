dc0c091160ba8f6ee1e1132239562d9b4e9b77e8
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-06-14 11:58:08 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.app.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnadd.setOnClickListener()
+        {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
