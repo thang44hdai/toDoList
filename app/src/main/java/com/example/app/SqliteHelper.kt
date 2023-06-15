57bf244827f40083d08e59998c97c966c67fde34
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-06-15 12:02:59 +0700

    Create SqliteHelper.kt

---
+++ app/src/main/java/com/example/app/SqliteHelper.kt
@@ -0,0 +1,19 @@
+package com.example.app
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SqliteHelper(context: Context): SQLiteOpenHelper(context, "USERDB", null,1) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        // tạo bảng
+        p0?.execSQL("CREATE TABLE users (id Integer PRIMARY KEY AUTOINCREMENT, name TEXT)")
+        // add data
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+
+
+}
