7d941f5bc73f8b109e7638c393683c87ad030018
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-06-15 12:02:53 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.app
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +14,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.ivavatar.setImageResource(R.drawable.ava)
         binding.btnadd.setOnClickListener()
         {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        var helper = SqliteHelper(applicationContext)
+        val db = helper.readableDatabase
+        var rs = db.rawQuery("SELECT * FROM users", null)
+        //
+        var ds = ArrayList<data>()
+        if(rs.moveToLast())
+            ds.add(data(rs.getString(1).toString(), "a2s"))
+        val adapter = adapter(ds)
+        binding.lvItem.adapter = adapter
+        binding.lvItem.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        super.onResume()
     }
 }
