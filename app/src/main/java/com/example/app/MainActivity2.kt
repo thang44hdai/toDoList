2697dfe9a8e73e0cbb5b6292acd54e4659461e9a
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-06-20 11:27:26 +0700

    Update MainActivity2.kt

--- app/src/main/java/com/example/app/MainActivity2.kt
+++ app/src/main/java/com/example/app/MainActivity2.kt
@@ -1,11 +1,14 @@
 package com.example.app
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.app.databinding.ActivityMain2Binding
+import java.util.Date
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
@@ -26,10 +29,17 @@ class MainActivity2 : AppCompatActivity() {
 
         binding.btnSave.setOnClickListener()
         {
+            var rs = db.rawQuery("SELECT * FROM users", null)
             var cv = ContentValues()
+            if(!rs.moveToLast())
+                cv.put("id", 1)
+            else
+                cv.put("id", rs.getString(0).toInt()+1)
             cv.put("name", binding.edtInput.text.toString())
+            cv.put("date", binding.tvDate.text.toString())
             db.insert("users", null, cv)
-            Toast.makeText(applicationContext,"ok", Toast.LENGTH_SHORT).show()
+            db.close()
+            Toast.makeText(applicationContext,"Đã lưu", Toast.LENGTH_SHORT).show()
             finish()
         }
 
@@ -39,6 +49,13 @@ class MainActivity2 : AppCompatActivity() {
             val selectionArgs = arrayOf(binding.edtDelete.text.toString())
             db.delete("users", selection, selectionArgs)
             finish()
+        }
+        binding.btnDate.setOnClickListener()
+        {
+            DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
+                datePicker, i, i2, i3 ->  binding.tvDate.setText("$i3 / ${i2 + 1} / $i")
+            },2022, 1, 1).show()
+            Toast.makeText(this, "okeee", Toast.LENGTH_SHORT).show()
         }
 
     }
